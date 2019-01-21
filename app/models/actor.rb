94080c495bab3a5d1922619806e941c542168780
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-01-21 20:11:38 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,19 +1,15 @@
-class Artist < ActiveRecord::Base
-  has_many :songs
-  has_many :genres, through: :songs
+class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
 
-  def get_genre_of_first_song
-    #return the genre of the artist's first saved song
-    self.songs.first.genre
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
-  def song_count
-    #return the number of songs associated with the artist
-    self.songs.count
+  def list_roles
+    characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
 
-  def genre_count
-    #return the number of genres associated with the artist
-    self.genres.count
-  end
 end
